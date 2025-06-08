78bc2aa3d6d84d1d18fede8fef15fe79d1f8f362
Adan Rezende <adan.rezende@gmail.com>
2025-06-08 10:53:35 -0300

    Update wsTranList.js

--- wsTranList.js
+++ wsTranList.js
@@ -1,6 +1,8 @@
-[{
-  TRAN_DATE: "2025-10-10",
-  TRAN_NAME: "aaa",
-  TRAN_STORE: "bbb",
-  TRAN_VALUE: 10.00
-}]
+{
+  data: [{
+    TRAN_DATE: "2025-10-10",
+    TRAN_NAME: "aaa",
+    TRAN_STORE: "bbb",
+    TRAN_VALUE: 10.00
+  }]
+}
